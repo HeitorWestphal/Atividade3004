3c4a9984064c3a44f6cac063cf27b3a76c3254ea
Pedro Nieto <pedronieto.2005@gmail.com>
2024-04-30 19:44:50 -0300

    fix: add Pessoa

--- src/main/java/model/Aluno.java
+++ src/main/java/model/Aluno.java
@@ -3,5 +3,30 @@ package model;
 public class Aluno extends Pessoa {
     private String curso;
     private int fase;
+
+    public Aluno() {
+        this(0, "", 0, "", 0);
+    }
     
+    public Aluno(int id, String nome, int idade, String curso, int fase) {
+        super(id, nome, idade);
+        this.curso = curso;
+        this.fase = fase;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public int getFase() {
+        return fase;
+    }
+
+    public void setFase(int fase) {
+        this.fase = fase;
+    }
 }
