1298c4205be9cd9be380b860e871f609b71a36ba
1072410915 <1072410915@UNS11DT099884.animaedu.intranet>
2024-04-30 20:08:04 -0300

    fix: adicionei visualização da tela de menu

--- src/main/java/principal/Principal.java
+++ src/main/java/principal/Principal.java
@@ -1,16 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package principal;
 
-/**
- *
- * @author 1072411483
- */
+import frames.FrmMenuMain;
+
 public class Principal {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+       FrmMenuMain tela = new  FrmMenuMain();
+       tela.setVisible(true);
     }
 }
